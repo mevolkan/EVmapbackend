8fc240cb14a589e1ae36a9219fc89794e18e4627
volkan <nzaro19@gmail.com>
2024-05-20 11:57:15 +0300

    cleanup

--- config.js
+++ config.js
@@ -1,12 +1,21 @@
-const dotenv = require('dotenv');
+const fs = require('fs');
 const path = require('path');
+const dotenv = require('dotenv');
 
+// Determine the environment file path
+const envFilePath = path.resolve(__dirname, `${process.env.NODE_ENV}.env`);
+const defaultEnvFilePath = path.resolve(__dirname, '.env');
+
+// Check if the environment-specific file exists, otherwise use the default .env file
+const finalEnvFilePath = fs.existsSync(envFilePath) ? envFilePath : defaultEnvFilePath;
+
+// Load the environment variables from the chosen file
 dotenv.config({
-    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+    path: finalEnvFilePath
 });
 
 module.exports = {
-    NODE_ENV : process.env.NODE_ENV ,
-    MONGO_URI : process.env.MONGO_URI ,
-    PORT : process.env.PORT,
-}
+    NODE_ENV: process.env.NODE_ENV,
+    MONGO_URI: process.env.MONGO_URI,
+    PORT: process.env.PORT,
+};
--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-let config = require('./config.js');
-let express = require('express');
-let bodyParser = require('body-parser');
-let cors = require('cors');
-let mode = '';
-let mongoose = require('mongoose');
+let config = require("./config.js");
+let express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let mode = "";
+let mongoose = require("mongoose");
 let app = express();
 
 console.log(`MONGO_URI= ${config.MONGO_URI}`);
 
-let apiRoutes = require("./api-routes")
+let apiRoutes = require("./api-routes");
 
 app.use(cors());
 
-var path = require('path');
+var path = require("path");
 
-app.use(express.static(__dirname + '/views'));
-
+app.use(express.static(__dirname + "/views"));
 
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-
-mongoose.connect(`${config.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Connected to mongo database");
-    })
-    .catch((err) => {
-        console.log("Error connecting mongo database", err);
-        process.exit(1);
-    });
+mongoose
+  .connect(`${config.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongo database");
+  })
+  .catch((err) => {
+    console.log("Error connecting mongo database", err);
+    process.exit(1);
+  });
 
 var db = mongoose.connection;
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(config.PORT, config.HOST, () => {
-    console.log(`APP LISTENING ON http://127.0.0.1:${config.PORT}`);
-})
+  console.log(`APP LISTENING ON http://127.0.0.1:${config.PORT}`);
+});
